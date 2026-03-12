9edbf2ce3f6a3022e17bf8aad50e25a9634a2581
Əliqoşqar Kərimli <145402717+aligoshgar@users.noreply.github.com>
2026-03-13 00:26:24 +0400

    Update javascripts.js

--- JS/javascripts.js
+++ JS/javascripts.js
@@ -2,7 +2,7 @@
    MOVIES data
    =========================== */
 const MOVIES = [
-  {title:"Eşref Rüya - 36. Bölüm", cover:"https://m.media-amazon.com/images/M/MV5BYzI5MjM5NDMtNTFjZC00ZTI0LWJjMWQtZjQyNzdiYWY2ZjUyXkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg", src:"https://www.dailymotion.com/video/xa1rrma", year:2025, genre:"Dram", id: "esrefruya", trend: "yes", special: "yes"},
+  {title:"Eşref Rüya - 36. Bölüm", cover:"https://m.media-amazon.com/images/M/MV5BYzI5MjM5NDMtNTFjZC00ZTI0LWJjMWQtZjQyNzdiYWY2ZjUyXkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg", src:"https://www.dailymotion.com/video/xa1sjjk", year:2025, genre:"Dram", id: "esrefruya", trend: "yes", special: "yes"},
   {title:"Beyblade: Bakuten Shoot", cover:"../beyblade/beyblade.jpg", src:"../beyblade", year:2000, genre:"Cizgi serial", id: "beyblade", trend: "yes", special: "yes"},
   {title:"Avatar 3: Ateş ve kül / Avatar 3: Fire and Ash", cover:"https://m.media-amazon.com/images/M/MV5BZDYxY2I1OGMtN2Y4MS00ZmU1LTgyNDAtODA0MzAyYjI0N2Y2XkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg", src:"https://drive.google.com/file/d/1N0RWUSVUpaBNZgZuJILpsKxcyv5np5WY/view?usp=drive_link", year:2025, genre:"Fantastik", id: "avatar3", trend: "yes", special: "yes"},
   {title:"Göz açıp kapayıncaya kadar / In the Blink of an Eye", cover:"https://m.media-amazon.com/images/M/MV5BYjNhNjQ2Y2ItMGM5ZC00OWE4LWEwZjItOWM5ZTdkYmYyZjE5XkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg", src:"https://vk.com/video359563763_456241275", year:2026, genre:"Dram", id: "intheblinkofaneye"},
