1462657fd034ac88bbc138f6a7da99988b0f5cfd
Əliqoşqar Kərimli <145402717+aligoshgar@users.noreply.github.com>
2026-04-09 14:55:44 +0400

    Update javascripts.js

--- JS/javascripts.js
+++ JS/javascripts.js
@@ -2,7 +2,7 @@
    MOVIES data
    =========================== */
 const MOVIES = [
-  {title:"Eşref Rüya - 40. Bölüm", cover:"https://m.media-amazon.com/images/M/MV5BYzI5MjM5NDMtNTFjZC00ZTI0LWJjMWQtZjQyNzdiYWY2ZjUyXkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg", src:"https://m.vkvideo.ru/video-236658385_456239061", year:2025, genre:"Dram", id: "esrefruya", trend: "yes", special: "yes"},
+  {title:"Eşref Rüya - 40. Bölüm", cover:"https://m.media-amazon.com/images/M/MV5BYzI5MjM5NDMtNTFjZC00ZTI0LWJjMWQtZjQyNzdiYWY2ZjUyXkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg", src:"https://m.vkvideo.ru/video787385537_456282949", year:2025, genre:"Dram", id: "esrefruya", trend: "yes", special: "yes"},
   {title:"Beyblade: Bakuten Shoot", cover:"../beyblade/beyblade.jpg", src:"../beyblade", year:2000, genre:"Cizgi serial", id: "beyblade", trend: "yes", special: "yes"},
   {title:"Sesimi hisset / Feel My Voice", cover:"https://m.media-amazon.com/images/M/MV5BZWZlNDE5ZTgtMGRmNi00ZmY1LTk3ZTQtNDdhYzM1OGQ2M2VlXkEyXkFqcGc@._V1_.jpg", src:"https://ok.ru/video/12919680273001", year:2026, genre:"Dram", id: "feelmyvoice"},
   {title:"Peaky Blinders: Ölümsüz adam / Peaky Blinders: The Immortal Man", cover:"https://m.media-amazon.com/images/M/MV5BNTdlNTNjNjctYTg2MC00NTFlLTliNTctODFiZjZmNWRkYTVlXkEyXkFqcGc@._V1_.jpg", src:"https://ok.ru/video/12445598354025", year:2026, genre:"Dram", id: "peakyblinders"},
