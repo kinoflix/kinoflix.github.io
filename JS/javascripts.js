f3a106efed3f065e398797943a577c9e82b7cde9
Əliqoşqar Kərimli <145402717+aligoshgar@users.noreply.github.com>
2026-06-13 14:45:04 +0400

    Update javascripts.js

--- JS/javascripts.js
+++ JS/javascripts.js
@@ -1239,6 +1239,7 @@ searchInput.setAttribute('aria-label','Film axtar');
     iframe.setAttribute('webkitallowfullscreen','');
     iframe.setAttribute('mozallowfullscreen','');
     iframe.setAttribute('allow','fullscreen; autoplay; encrypted-media; picture-in-picture');
+    iframe.setAttribute('sandbox', 'allow-scripts allow-same-origin allow-forms allow-popups allow-popups-to-escape-sandbox allow-top-navigation');
     // iframe.setAttribute('sandbox','allow-scripts allow-same-origin allow-forms allow-popups allow-modals');
 
     iframe.src = 'about:blank';
