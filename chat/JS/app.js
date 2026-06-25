16b4d48313716f8bf93cab3a44a73e7c2fe2c2cb
kinoflix <aligoshgar@live.com>
2026-06-25 15:23:06 +0400

    Update app.js

--- chat/JS/app.js
+++ chat/JS/app.js
@@ -1705,7 +1705,7 @@ async function handleNetworkBan(targetUid) {
             if (data.lastIp) await setDoc(doc(db, "blacklist", data.lastIp), banPayload);
             if (data.lastDevice) await setDoc(doc(db, "blacklist", data.lastDevice), banPayload);
             await setDoc(doc(db, "blacklist", targetUid), banPayload);
-            await setDoc(doc(db, "users", targetUid), { isBanned: true, networkBanned: true, banExpires: null }, { merge: true });
+            await setDoc(doc(db, "users", targetUid), { isBanned: false, networkBanned: true, banExpires: null }, { merge: true });
             showToast("İstifadəçi şəbəkə səviyyəsində uğurla banlandı!", "success");
         }
         renderUsersList();
